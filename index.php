df6a355182df4a07f6f4840d493fa350feb48bea
studentdiary <76680383+studentdiary@users.noreply.github.com>
2020-12-28 13:57:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,13 @@ if (!empty($_SERVER['HTTPS'])&&('on' == $_SERVER['HTTPS']))
  $uri ='http://';
   }
       $uri .= $_SERVER[HTTP_HOST'];
-      header('Location: '.$uri. '/dashboard/');
+     <html lang="en" class="html-fluid">
+     <head>
+     <meta http-equiv="Content-Type" content="text/html;
+     charset=UTF-8">
+      </head>
+      <body>
+      <p>test1</p>
       exit;
       ?>
+      
