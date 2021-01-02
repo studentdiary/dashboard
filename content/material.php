9fd43ddd7dcd30ee323b971c7fc6327b1ad4ca4f
studentdiary <76680383+studentdiary@users.noreply.github.com>
2021-01-03 03:02:51 +0530

    Update material.php

--- content/material.php
+++ content/material.php
@@ -1,3 +1,134 @@
-<?php 
-hello world;
-?php>
+<?php include 'head.php';?>
+<div class="infoa">
+<ul id="myUL">
+    <li><span class="caret">subjects</span>
+      <ul class="nested">
+        <li><span class="caret">2nd year 1st sem</span>
+            <ul class="nested">
+        <li><span class="caret">DLDMP</span>
+          <ul class="nested">
+            <li>available soon</li>
+            <li>available soon</li>
+            <li>available soon</li>
+            <li>available soon</li>
+            <li>available soon</li>
+            <li>available soon</li>
+            <li>available soon</li>
+            <li>available soon</li>
+            <li>available soon</li>
+            <li>available soon</li>
+            </ul></li>
+            <li><span class="caret">Computer organsation and arhitecture</span>
+                <ul class="nested">
+                  <li>computer type</li>
+                  <li>functional units</li>
+                  <li>Basic operational concepts</li>
+                  <li>available soon</li>
+                  <li>available soon</li>
+                  <li>available soon</li>
+                  <li>available soon</li>
+                  <li>available soon</li>
+                  <li>available soon</li>
+                  <li>available soon</li>
+                  <li>available soon</li>
+                  <li>available soon</li>
+                  <li>available soon</li>
+          </ul>
+        </li>  
+      </ul>
+    </li>
+  </ul></div>
+<div class="col-container">
+    <div class="col" style="background:orange">
+      <h2>ES</h2>
+      <a href="#">
+        <li>Available soon</li></a>
+        <a href="#">
+          <li>*1st chapter</li></a>
+          <a href="#">
+                  <li>2nd chapter</li></a>
+                  <a href="#">
+                          <li>3rd chapter</li></a>
+                          <a href="#">
+                                  <li>4rth chapter</li></a>
+                                  <a href="#">
+                                          <li>5th chapter</li></a>
+  </div>
+    <div class="col" style="background:yellow">
+      <h2>C++</h2>
+      <a href="https://drive.google.com/file/d/1g99ZTBS5VW5jJD8zwNeVI36yDdrImHv8/view?usp=sharing">
+        <li>a complete guide programming in c++</li></a>
+        <a href="#">
+                <li>*1st chapter</li></a>
+                <a href="#">
+                        <li>2nd chapter</li></a>
+                        <a href="#">
+                                <li>3rd chapter</li></a>
+                                <a href="#">
+                                        <li>4rth chapter</li></a>
+                                        <a href="#">
+                                                <li>5th chapter</li></a>
+    </div>
+  
+    <div class="col" style="background:rgb(111, 124, 113)">
+      <h2>DLDMP</h2>
+      <a href="https://drive.google.com/file/d/1fLd7QnjVdjLwmD-u_TuXPdkp2lR90qZu/view?usp=sharing">
+              <li>1st unit notes</li></a>
+              <a href="https://drive.google.com/file/d/1fLd7QnjVdjLwmD-u_TuXPdkp2lR90qZu/view?usp=sharing">
+                      <li>2nd unit notes</li></a>
+                      <a href="https://drive.google.com/file/d/1x156wX3F5la0J0x-bCo3Ft8tU8Tw2MCk/view?usp=sharing">
+                              <li>3rd Unit  notes</li></a>
+                              <a href="#">
+                                     <li>4rth chapter</li></a>
+                                        <a href="#">
+                                                <li>5th chapter</li></a>
+    </div>
+    </div>
+  <div class="col-container">
+    <div class="col" style="background:orange">
+      <h2>ES</h2>
+      <a href="#">
+        <li>Available soon</li></a>
+        <a href="#">
+          <li>*1st chapter</li></a>
+          <a href="#">
+                  <li>2nd chapter</li></a>
+                  <a href="#">
+                          <li>3rd chapter</li></a>
+                          <a href="#">
+                                  <li>4rth chapter</li></a>
+                                  <a href="#">
+                                          <li>5th chapter</li></a>
+  </div>
+    <div class="col" style="background:yellow">
+      <h2>C++</h2>
+      <a href="https://drive.google.com/file/d/1g99ZTBS5VW5jJD8zwNeVI36yDdrImHv8/view?usp=sharing">
+        <li>a complete guide programming in c++</li></a>
+        <a href="#">
+                <li>*1st chapter</li></a>
+                <a href="#">
+                        <li>2nd chapter</li></a>
+                        <a href="#">
+                                <li>3rd chapter</li></a>
+                                <a href="#">
+                                        <li>4rth chapter</li></a>
+                                        <a href="#">
+                                                <li>5th chapter</li></a>
+    </div>
+  
+    <div class="col" style="background:rgb(111, 124, 113)">
+      <h2>DLDMP</h2>
+      <a href="https://drive.google.com/file/d/1fLd7QnjVdjLwmD-u_TuXPdkp2lR90qZu/view?usp=sharing">
+              <li>1st unit notes</li></a>
+              <a href="https://drive.google.com/file/d/1fLd7QnjVdjLwmD-u_TuXPdkp2lR90qZu/view?usp=sharing">
+                      <li>2nd unit notes</li></a>
+                      <a href="https://drive.google.com/file/d/1x156wX3F5la0J0x-bCo3Ft8tU8Tw2MCk/view?usp=sharing">
+                              <li>3rd Unit  notes</li></a>
+                              <a href="#">
+                                     <li>4rth chapter</li></a>
+                                        <a href="#">
+                                                <li>5th chapter</li></a>
+    </div>
+    </div>
+  <?php include 'foot.php';
+  ?>
