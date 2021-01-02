f7b574ceb3ca575c2e116be757a9b65f2d9d6e78
studentdiary <76680383+studentdiary@users.noreply.github.com>
2021-01-03 03:13:16 +0530

    Delete foot.php

--- content/foot.php
+++
@@ -1,9 +0,0 @@
-
-  <div class="sub_header" style="font-family: 'caveat';color:rgb(255, 245, 245);background-color:black;text-align:center;height:auto;">
-  <h4>A product of student diary</h4>
-  <a href="https://www.facebook.com/Student-Diary-103657885012335"style="color:white;"><i class="fa fa-facebook" aria-hidden="true">Facebook</i></a>
-  <a href="https://instagram.com/techie_tune?igshid=10z5100dcztl4"style="color:white;"><i class="fa fa-instagram" aria-hidden="true">Instagram</i></a>
-  <a href="https://t.me/joinchat/Sq0IZSzOxxKYaJuA"style="color:white;"><i class="fa fa-telegram" aria-hidden="true"> Telegram</i></a>
-  <p><i class="fa fa-copyright" aria-hidden="true"></i>copyright 2021</p>
-  <h4><img src="https://github.githubassets.com/images/icons/emoji/octocat.png">THANKS TO GITHUB</h4>
-</div>
