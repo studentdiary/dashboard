403b6521867008deac731d3dfcd528e6455d91f9
studentdiary <76680383+studentdiary@users.noreply.github.com>
2021-01-03 02:58:20 +0530

    Add files via upload

---
+++ content/foot.php
@@ -0,0 +1,14 @@
+<div class="material_info"style="font-family: 'Charmonman';color:black;text-align:center;border-radius:32px;"> 
+    <h4><i>Material link google Drive  and category wise collection page.</i> </h4>
+    <a href="https://drive.google.com/drive/folders/1LtPHOQll5ko0Fz09WOFi9WDsy4u3J9gb?usp=sharing">
+    <img src="https://www.lifewire.com/thmb/uevGm8GmYjR8UwXH0pkAl4xmexM=/960x640/filters:no_upscale():max_bytes(150000):strip_icc()/download-7105085d68424c93a47bcc6228d946fa.png"alt="GDRIVE" border-radius="302px" float="right" height="140" width="220"></a>
+    <a href="content/material.php"><img src="https://www.pngkey.com/png/full/326-3266040_spiral-note-book-a6-spirax-hardcover-notebook-a5.png"alt ="Material"border-radius="302px" float="right" height="140" width="220"></a>  
+  </div>
+  <div class="sub_header" style="font-family: 'caveat';color:rgb(255, 245, 245);background-color:black;text-align:center;height:auto;">
+  <h4>A product of student diary</h4>
+  <a href="https://www.facebook.com/Student-Diary-103657885012335"style="color:white;"><i class="fa fa-facebook" aria-hidden="true">Facebook</i></a>
+  <a href="https://instagram.com/techie_tune?igshid=10z5100dcztl4"style="color:white;"><i class="fa fa-instagram" aria-hidden="true">Instagram</a></i>
+  <a href="https://t.me/joinchat/Sq0IZSzOxxKYaJuA"style="color:white;"><i class="fa fa-telegram" aria-hidden="true"> Telegram</a></i>
+  <p><i class="fa fa-copyright" aria-hidden="true"></i>copyright 2021</p>
+  <h4><img src="https://github.githubassets.com/images/icons/emoji/octocat.png">THANKS TO GITHUB</h4>
+</div>
